49728da59af58b0cad6b57c95324cdf575b7d3c0
Juliano <julianosoder1989@gmail.com>
2021-08-29 10:31:15 -0300

    feat removing css folder

--- src/app/meuComponent.ts
+++ src/app/meuComponent.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'new-root',
   templateUrl: './components/newComponent.html',
-  styleUrls: ['../css/app.component.sass'],
+  styleUrls: ['./app.component.sass'],
 })
 
 export class AppComponent2 {
